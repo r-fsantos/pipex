1a6e5b07150ac2c2c7dfca6c66821087f7b7dc2a
renato <felicio95.jr@gmail.com>
2022-09-28 22:57:13 -0300

    refactor: main process should wait for all children

--- src/main.c
+++ src/main.c
@@ -6,16 +6,16 @@
 /*   By: rfelicio <rfelicio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 10:10:22 by rfelicio          #+#    #+#             */
-/*   Updated: 2022/09/28 18:11:26 by rfelicio         ###   ########.fr       */
+/*   Updated: 2022/09/28 21:57:38 by rfelicio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_pipex.h"
 
-// TODO: Refactor ft_error. It is possible to set fl_error on t_env instance
 int	main(int argc, char **argv, char **envp)
 {
 	t_env	env;
+	int		i;
 
 	if (argc != EXPECTED_ARGC)
 		ft_error(e_bad_input, NULL);
@@ -27,6 +27,9 @@ int	main(int argc, char **argv, char **envp)
 		ft_error(env.fl_error, &env);
 	if (!ft_executing_outfile_pipeline(1, &env))
 		ft_error(env.fl_error, &env);
+	i = -1;
+	while (++i < env.argc)
+		wait(NULL);
 	ft_mdealloc(&env);
 	return (0);
 }
